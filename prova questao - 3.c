f35f48b6410a0893033b98cf956dfb907aaac074
lelafran <isafranceschi01@gmail.com>
2025-04-24 18:13:02 -0300

    Add files via upload

---
+++ prova questao - 3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    float forca, nivel;
+    int mult;
+
+    printf("Digite a forca do personagem:");
+    scanf("%f", &forca);
+
+    printf("Digite o nivel da arma:");
+    scanf("%f", &nivel);
+
+    printf("Digite o multiplicador critico:");
+    scanf("%d", &mult);
+
+
+}
