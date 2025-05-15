9a1d4698b4cc159eb589c37539b46494c8c0af8f
lelafran <isafranceschi01@gmail.com>
2025-05-15 16:26:37 -0300

    Add files via upload

---
+++ funções1final.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int arredonda (float x);
+int main ()
+{
+    float valor;
+    int  valor_inteiro;
+
+    printf("Digite um valor:");
+    scanf("%f", &valor);
+
+    valor_inteiro = arredonda (valor);
+    printf("%d", valor_inteiro);
+
+}
+
+int arredonda (float valor)
+{
+    if (valor > 0)
+        return (int) (valor + 0.5);
+    else
+        return (int) (valor - 0.5);
+}
+
